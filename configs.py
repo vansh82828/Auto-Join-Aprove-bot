e8a3425c13fd6a7781b68991d096487cecee741a
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2023-12-06 11:38:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,11 +8,11 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "15657755"))
     API_HASH = getenv("API_HASH", "7cce51d4664d010b90ad690e0d5121ad")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6678430839:AAELqD5bQMwG9M_wXoQMv-Le4G_VBq8-lSM")
-    FSUB = getenv("FSUB", "TagaruPalya_kannad_movie_12")
-    CHID = int(getenv("CHID", "-1001526945133"))
-    SUDO = list(map(int, getenv("SUDO", "5003133371").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6900717691:AAHTC2hKMhNjEJqEKUIbsE-VkdnGB6a5dMA")
+    FSUB = getenv("FSUB", "PS_BOTz")
+    CHID = int(getenv("CHID", "-1001910352559"))
+    SUDO = list(map(int, getenv("SUDO", "5003133371 950958796").split()))
+    MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
 
