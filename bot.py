6c96a945ab5b8b57b148c58abe9515b8b41a624b
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2023-12-06 11:20:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @SUDIPSAHA06 __**".format(m.from_user.mention, m.chat.title))
+        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @PS_BOTz __**".format(m.from_user.mention, m.chat.title))
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
@@ -59,21 +59,21 @@ async def op(_, m :Message):
             keyboard = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🗯 Channel", url="https://t.me/main_channel_bot_update"),
-                        InlineKeyboardButton("💬 Support", url="https://t.me/Ott_Movie_Request_Group")
+                        InlineKeyboardButton("🗯 Channel", url="https://t.me/PS_OTT"),
+                        InlineKeyboardButton("💬 Support", url="https://t.me/PS_BOTz")
                     ],[
-                        InlineKeyboardButton("➕ Add me to your Chat ➕", url="http://t.me/AUTO_APPROVED_REQUEST_BOT?startgroup")
+                        InlineKeyboardButton("➕ Add me to your Chat ➕", url="http://t.me/Auto_psbot?startgroup")
                     ]
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://graph.org/file/6739040d7d63179dea3f6.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @SUDIPSAHA06 __**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+            await m.reply_photo("https://graph.org/file/6739040d7d63179dea3f6.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @PS_BOTz __**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
     
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("💁‍♂️ Start me private 💁‍♂️", url="http://t.me/AUTO_APPROVED_REQUEST_BOT?startgroup")
+                        InlineKeyboardButton("💁‍♂️ Start me private 💁‍♂️", url="http://t.me/Auto_psbot?startgroup")
                     ]
                 ]
             )
@@ -101,10 +101,10 @@ async def chk(_, cb : CallbackQuery):
             keyboard = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🗯 Channel", url="https://t.me/main_channel_bot_update"),
-                        InlineKeyboardButton("💬 Support", url="https://t.me/Ott_Movie_Request_Group")
+                        InlineKeyboardButton("🗯 Channel", url="https://t.me/PS_OTT"),
+                        InlineKeyboardButton("💬 Support", url="https://t.me/PS_BOTz")
                     ],[
-                        InlineKeyboardButton("➕ Add me to your Chat ➕", url="http://t.me/AUTO_APPROVED_REQUEST_BOT?startgroup")
+                        InlineKeyboardButton("➕ Add me to your Chat ➕", url="http://t.me/Auto_psbot?startgroup")
                     ]
                 ]
             )
